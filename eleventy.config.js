64f2e958dc2098d401c77b3284b107357005ba94
Lacey Guthrie <lacey@fuelmade.com>
2026-05-20 12:23:23 -0400

    new tooling and dev process

---
+++ eleventy.config.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets straight through to _site/ without processing.
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/*.png");
+  eleventyConfig.addPassthroughCopy("src/*.ico");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
+};
