3ed6a3c8e26194c8cc87b14150cac9bbe57e7b26
Lacey Guthrie <lacey@fuelmade.com>
2026-07-29 14:41:20 -0400

    updates to structure / cleaning

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,11 +6,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/*.png");
   eleventyConfig.addPassthroughCopy("src/*.ico");
 
-  // Serialize a value to a JSON string (used to inline data into pages).
-  eleventyConfig.addFilter("json", function (value) {
-    return JSON.stringify(value).replace(/</g, "\\u003c");
-  });
-
   return {
     dir: {
       input: "src",
